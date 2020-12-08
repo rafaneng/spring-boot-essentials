7f9f846a9400eab3c4a50322bcb9c7cad61104d8
Rafael Santos <rafaneng@gmail.com>
2020-12-08 19:01:23 -0300

    validacao de campos obrigatórios

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AnimePostRequestBody {
+    @NotEmpty(message = "O campo nome não pode estar vazio.")
     private String name;
 }
